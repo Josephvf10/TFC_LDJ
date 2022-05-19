40644967aa5c6de50499b9e4ca02dad9a163d22c
Joseph <josephvz17@gmail.com>
2022-05-19 16:47:00 +0200

    añadido de Splash

---
+++ Proyectotfc/app/src/main/java/com/dam/proyectotfc/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.dam.proyectotfc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask t = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(t,1500);
+    }
+}
